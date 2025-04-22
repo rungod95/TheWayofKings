9e75d5f6f1a49065872966401869775d479e45df
javier <jabierplanos@gmail.com>
2025-04-22 17:06:39 +0200

    "Añadido plataforma movil  y checkpoint"

---
+++ assets/maps/tileset2.tsx
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset2" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="tileset2.png" width="1024" height="1024"/>
+ <tile id="1284" type="hazard"/>
+ <tile id="1285" type="hazard"/>
+ <tile id="1347" type="hazard"/>
+ <tile id="1348" type="hazard"/>
+ <tile id="1349" type="hazard"/>
+ <tile id="1380" type="hazard"/>
+ <tile id="1411" type="hazard"/>
+ <tile id="1412" type="hazard">
+  <animation>
+   <frame tileid="1413" duration="1500"/>
+   <frame tileid="1411" duration="1500"/>
+   <frame tileid="1412" duration="1500"/>
+   <frame tileid="1476" duration="1500"/>
+  </animation>
+ </tile>
+ <tile id="1413" type="hazard"/>
+ <tile id="1444" type="hazard"/>
+ <tile id="1475" type="hazard"/>
+ <tile id="1476" type="hazard"/>
+ <tile id="1477" type="hazard"/>
+ <tile id="1508" type="hazard"/>
+ <tile id="1572" type="hazard"/>
+ <tile id="1616" type="fire"/>
+</tileset>
